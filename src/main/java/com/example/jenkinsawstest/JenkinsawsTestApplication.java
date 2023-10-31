78c04007aa24fda78b732102400ed881429d022a
gaurav sharma <129361820+Gaurav-Sharma98@users.noreply.github.com>
2023-10-31 17:40:23 +0530

    Update JenkinsawsTestApplication.java

--- src/main/java/com/example/jenkinsawstest/JenkinsawsTestApplication.java
+++ src/main/java/com/example/jenkinsawstest/JenkinsawsTestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsawsTestApplication {
 
     public static void main(String[] args) {
-        System.out.println("Application running!!!!******************");
+        System.out.println("Application running!!!!*******************");
         SpringApplication.run(JenkinsawsTestApplication.class, args);
     }
 
