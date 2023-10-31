46de7a5f2932ebd7acbceb00b42a6cfadc4b015d
gaurav sharma <129361820+Gaurav-Sharma98@users.noreply.github.com>
2023-10-31 17:39:34 +0530

    Initial commit

---
+++ src/main/java/com/example/jenkinsawstest/JenkinsawsTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.jenkinsawstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsawsTestApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Application running!!!!******************");
+        SpringApplication.run(JenkinsawsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkinsawstest/JenkinsawsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsawstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsawsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
